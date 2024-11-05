00a40cb44140b0463ba46dcaa0a4db7a9be2b5ef
BikashKumar <bikashcool481@gmail.com>
2024-11-05 23:13:57 +0530

    add create, get, update

---
+++ controllers/updateTodo.js
@@ -0,0 +1,31 @@
+// import the Model
+const Todo = require("../models/Todo");
+
+// define the router
+exports.updateTodo = async(req,res) => {
+    try{
+        const {id} = req.params;
+        const {title, description} = req.body;
+
+        const todo = await Todo.findByIdAndUpdate(
+            {_id:id},
+            {title, description, updatedAt:Date.now()},
+            {new:true},
+        )
+
+        res.status(200).json({
+            success:true,
+            data:todo,
+            message: `Updated Succesfully`,
+        })
+    }
+    catch(err){
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message: "Server Error",
+        });
+    }
+}
